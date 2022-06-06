468d819ce2d381eebc84a6427160788163943bc0
LopezBanos <sergioloba98@gmail.com>
2022-06-07 00:52:41 +0200

    Module1

---
+++ Modulo1/progs/main2.py
@@ -0,0 +1,12 @@
+from sys import path
+path.append('/Users/sergiolopezbanos/Documents/GitHub/Python/Python---PCAP/Modulo1/packages')
+
+import extra.iota
+print(extra.iota.FunI())
+
+import extra.good.best.sigma
+from extra.good.best.tau import funT
+
+print(extra.good.best.sigma.funS())
+print(funT())
+
